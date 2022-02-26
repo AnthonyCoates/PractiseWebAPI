2b35c2324233078bcc07c3e2135d77dab708ffef
Anthony Coates <anthjcoates@gmail.com>
2022-02-26 18:15:12 +0000

    Updated launch URL, added model class, added and registered DB context, scaffolded TodoItems controller, updated HttpPost create method

--- PractiseWebAPI/Program.cs
+++ PractiseWebAPI/Program.cs
@@ -1,19 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using PractiseWebAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new() { Title = "PractiseWebAPI", Version = "v1" });
+//});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PractiseWebAPI v1"));
 }
 
 app.UseHttpsRedirection();
